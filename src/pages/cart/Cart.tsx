357d97eb17b746eb9ace7718b62238656e92a51c
Your Name <youremail@example.com>
2026-06-30 18:02:05 +0300

    saves

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -1,7 +1,32 @@
+import { useContext } from 'react';
 import './ui/Cart.css';
+import AppContext from '../../features/_context/AppContext';
 
 export default function Cart() {
-    return <h1>Cart</h1>;
+    const {cart} = useContext(AppContext);
+
+    return <div className='row mx-3'>
+
+        <div className='col col-8'>
+            <h1>Shopping Cart</h1>
+            {cart.length == 0 
+            ? <p>Cart is empty</p>
+            : cart.map(ci => <div key={ci.product.id} className='row mb-3'>
+                <div className='col col-2'>
+                    <img src={ci.product.imageUrl} className='w-100' alt={ci.product.name} />
+                </div>
+                <div className='col col-5'>{ci.product.name}</div>
+                <div className='col col-1'>{ci.product.price}</div>
+                <div className='col col-3'>{ci.quantity}</div>
+                <div className='col col-1'>{ci.product.price * ci.quantity}</div>
+            </div>)}
+        </div>
+
+        <div className='col col-4 bg-light'>
+            <h2>Order summary</h2>
+        </div>
+
+    </div>;
 }
 /*
 Д.З. На сторінці товарної групи замінити заголовок
